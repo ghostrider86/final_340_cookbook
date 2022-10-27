3ec8193e3d905a65bfcbde12e3f7f38adf45afad
CIT 230 <elz20002@byui.edu>
2022-10-26 19:54:48 -0600

    final touches

--- _tests_/get.spec.js
+++ _tests_/get.spec.js
@@ -7,13 +7,30 @@ const request = supertest(app)
 describe('Test Handlers', () => {
     test('responds to /', async () => {
         const res = await request.get('/');
-        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(res.header['Access-Control-Allow-Origin', '*']).toBe('application/json; charset=utf-8');
         expect(res.statusCode).toBe(200)
     })
 
+    test('responds to /comments', async () => {
+        const res = await request.get('/comments');
+        expect(res.header['Access-Control-Allow-Origin', '*']).toBe('application/json; charset=utf-8');
+        expect(res.statusCode).toBe(200)
+    })
+
+    test('responds to /images', async () => {
+        const res = await request.get('/images');
+        expect(res.header['Access-Control-Allow-Origin', '*']).toBe('application/json; charset=utf-8');
+        expect(res.statusCode).toBe(200)
+    })
+
+    test('responds to /recipes', async () => {
+        const res = await request.get('/recipes');
+        expect(res.header['Access-Control-Allow-Origin', '*']).toBe('application/json; charset=utf-8');
+        expect(res.statusCode).toBe(200)
+    })
     test('responds to /user', async () => {
         const res = await request.get('/user');
-        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(res.header['Access-Control-Allow-Origin', '*']).toBe('application/json; charset=utf-8');
         expect(res.statusCode).toBe(200)
     })
 })
